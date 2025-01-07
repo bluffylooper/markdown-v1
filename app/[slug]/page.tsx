fc9fcb7185b27b0162f1e2f7a9a59520c0be5e5a
bluffylooper <bluffylooper@gmail>
2025-01-07 14:07:24 +0700

    fff

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,21 +1,21 @@
-import { notFound } from 'next/navigation'
+import { notFound } from "next/navigation";
 
-import { MDXContent } from '@/components/mdx-content'
-import { pages } from '#site/content'
+import { MDXContent } from "@/components/mdx-content";
+import { pages } from "#site/content";
 
 type Props = {
-  params: Promise<{ slug: string }>
-}
+  params: Promise<{ slug: string }>;
+};
 
 function getPageBySlug(slug: string) {
-  return pages.find(page => page.slug === slug)
+  return pages.find((page) => page.slug === slug);
 }
 
 export default async function PagePage({ params }: Props) {
-  const { slug } = await params
-  const page = getPageBySlug(slug)
+  const { slug } = await params;
+  const page = getPageBySlug(slug);
 
-  if (page == null) notFound()
+  if (page == null) notFound();
 
   return (
     <article className="prose dark:prose-invert py-6">
@@ -23,16 +23,16 @@ export default async function PagePage({ params }: Props) {
       <hr />
       <MDXContent code={page.body} name="props for mdx" />
     </article>
-  )
+  );
 }
 
 export async function generateMetadata({ params }: Props) {
-  const { slug } = await params
-  const page = getPageBySlug(slug)
-  if (page == null) return {}
-  return { title: page.title }
+  const { slug } = await params;
+  const page = getPageBySlug(slug);
+  if (page == null) return {};
+  return { title: page.title };
 }
 
 export function generateStaticParams() {
-  return pages.map(page => ({ slug: page.slug }))
+  return pages.map((page) => ({ slug: page.slug }));
 }
